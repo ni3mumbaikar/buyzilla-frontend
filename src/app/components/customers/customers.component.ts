65ba8ed5cc3dac1c2e3f603e4bc5fbc60bd67de8
Nitin Kale <nitinkale035@gmail.com>
2023-01-04 20:41:02 +0530

    Fix : customer update

--- src/app/components/customers/customers.component.ts
+++ src/app/components/customers/customers.component.ts
@@ -16,90 +16,90 @@ export class CustomersComponent {
   @ViewChild('customersform', { static: true }) form!: ElementRef;
   @ViewChild('customersmodal', { static: true }) modal!: ElementRef;
   @ViewChild('submitbtnmodal', { static: true }) btn!: ElementRef;
-  
-  formElement?:HTMLFormElement;
+
+  formElement?: HTMLFormElement;
 
   constructor(private customerService: ApiCustomersService) {
     customerService.get().subscribe(customers => this.customers = customers);
     this.tempCustomer = {
-      address:"",
-      city:"",
-      country:"",
-      customerID:0,
-      customerName:"",
-      postalCode:undefined
+      address: "",
+      city: "",
+      country: "",
+      customerID: 0,
+      customerName: "",
+      postalCode: undefined
     }
   }
 
-  loadCustomers(){
+  loadCustomers() {
     this.customerService.get().subscribe(customers => this.customers = customers);
   }
 
-  addNewCustomer(){
+  addNewCustomer() {
     this.tempCustomer = {
-      address:"",
-      city:"",
-      country:"",
-      customerID:0,
-      customerName:"",
-      postalCode:undefined
+      address: "",
+      city: "",
+      country: "",
+      customerID: 0,
+      customerName: "",
+      postalCode: undefined
     }
     this.openmodal(false);
   }
 
-  deleteCustomer(customer: Customer){
-      Swal.fire({
-        title: 'Delete Customer',
-        text: "Are you sure that you want to delete " + customer.customerName,
-        icon: 'question',
-        showCancelButton: true,
-        confirmButtonColor: '#3085d6',
-        cancelButtonColor: '#d33',
-        confirmButtonText: 'Yes, delete it!'
-      }).then((result) => {
-        if (result.isConfirmed) {
-          this.customerService.delete(customer.customerID!).subscribe(res => {
-            if (res['ok']) {
-              defaultMixin.fire({
-                icon: 'success',
-                title: 'Shipper ' + customer.customerName + ' removed.'
-              })
-              this.loadCustomers();
-            }
-          })
-        }
-      })
+  deleteCustomer(customer: Customer) {
+    Swal.fire({
+      title: 'Delete Customer',
+      text: "Are you sure that you want to delete " + customer.customerName,
+      icon: 'question',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Yes, delete it!'
+    }).then((result) => {
+      if (result.isConfirmed) {
+        this.customerService.delete(customer.customerID!).subscribe(res => {
+          if (res['ok']) {
+            defaultMixin.fire({
+              icon: 'success',
+              title: 'Shipper ' + customer.customerName + ' removed.'
+            })
+            this.loadCustomers();
+          }
+        })
+      }
+    })
   }
 
-  editCustomer(customer: Customer){
+  editCustomer(customer: Customer) {
     this.tempCustomer = customer
-    this.openmodal(false);
+    this.openmodal(true);
   }
 
-  openmodal(edit:boolean){
+  openmodal(edit: boolean) {
     (this.modal.nativeElement as HTMLElement).style.display = 'inline-flex';
     this.formElement = this.form.nativeElement as HTMLFormElement;
     this.customerService.get().subscribe(customers => this.customers = customers);
     this.formElement.onsubmit = () => {
-      if(!edit){
-        this.customerService.post(this.tempCustomer).subscribe(res=>{
-          if(res['ok']){
+      if (!edit) {
+        this.customerService.post(this.tempCustomer).subscribe(res => {
+          if (res['ok']) {
             defaultMixin.fire({
-              icon:'success',
-              text:'User is registered'
+              icon: 'success',
+              text: 'User is registered'
             })
             this.closemodal();
           }
         });
       } else {
-        this.customerService.put(this.tempCustomer).subscribe(res=>{
-          if(res['ok']){
+        this.customerService.put(this.tempCustomer).subscribe(res => {
+          if (res['ok']) {
             defaultMixin.fire({
-              icon:'success',
-              text:'User is registered'
+              icon: 'success',
+              text: 'User is updated'
             })
-            this.closemodal();
           }
+          this.closemodal();
         });
       }
     }
